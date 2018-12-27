cca214b92cc3e09be4b75724e937245b21444bf6
vagrant <vagrant@homestead>
2018-12-27 01:02:44 +0000

    create date

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Carbon\Carbon;
 
 class ImportController extends Controller
 {
@@ -31,7 +32,30 @@ class ImportController extends Controller
 		$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
 		$reader->setReadDataOnly(true);
 		$spreadsheet = $reader->load($pathFileName);
-		dd($spreadsheet->getActiveSheet()->toArray(null, true, true, true));
+		$mes = $spreadsheet->getActiveSheet()->getCell('B1')->getValue();
+		$mess = Carbon::parse($mes)->month;
+		$anio = $spreadsheet->getActiveSheet()->getCell('C1')->getValue();
+		$dataArray = $spreadsheet->getActiveSheet()
+		    ->rangeToArray(
+		        'A2:AG20',     // The worksheet range that we want to retrieve
+		        TRUE,        // Value that should be returned for empty cells
+		        TRUE,        // Should formulas be calculated (the equivalent of getCalculatedValue() for each cell)
+		        TRUE,        // Should values be formatted (the equivalent of getFormattedValue() for each cell)
+		        TRUE         // Should the array be indexed by cell row and cell column
+		    );
+			var_dump($mes, $anio);
+		    foreach ($dataArray as $row) {
+		    	//var_dump($row);
+		    	if (isset($row)) {
+		    		for ($i=1; $i <32 ; $i++) { 
+		    		$fecha = Carbon::create($anio, 07, $i);
+		    		var_dump($fecha);
+		    	}
+		    }
+		    	
+		    }
+
+		//dd($mes, $anio, $dataArray);
 
 	}
 
